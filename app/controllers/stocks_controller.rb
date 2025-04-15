9712c60b03f7d337a34ac831e6ddc54554060dbb
Angelo01623 <rafanan.michaelangelo015@gmail.com>
2025-04-15 16:36:20 +0800

    Inventory: Add Validation in Dropdown Supplier

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,71 +5,108 @@ class StocksController < ApplicationController
     @subheader_side_actions = [
       {
         id: "btn-new",
-        link: new_inventory_path, 
+        link: new_inventory_path,
         class: "fa fa-plus",
         text: "New"
       }
     ]
-    @inventories = Inventory.all || []  
+    @inventories = Inventory.all || []
   end
 
   def view
-    @inventory = Inventory.find_by(id: params[:id]) 
-  
+    @inventory = Inventory.find_by(id: params[:id])
+    @brands = Brand.all
+
     if @inventory.nil?
-      redirect_to stocks_path, alert: "Inventory not found"  
+      redirect_to stocks_path, alert: "Inventory not found"
       return
     end
-  
+
     @subheader_side_actions = [
       {
         id: "btn-distribute",
-        link: new_distribute_path(inventory_id: @inventory.id), 
+        link: new_distribute_path(inventory_id: @inventory.id),
         class: "fa fa-share",
         text: "Distribute"
       }
     ]
   end
-  
+
   def new
     @inventory = Inventory.new
-    @inventory.data ||= {}  
+    @inventory.data ||= {}
     @items = Item.all
     @suppliers = Supplier.all
+    @brands = Brand.all
   end
-  
+
   def create
     @inventory = Inventory.new(inventory_params)
 
+    @inventory.data = {
+      "title" => params[:inventory][:title],
+      "brand_id" => params[:inventory][:brand_id],
+      "model" => params[:inventory][:model],
+      "threshhold_alerts" => params[:inventory][:threshhold_alerts],
+      "reorder_points" => params[:inventory][:reorder_points]
+    }
 
-    @inventory.data = { "title" => params[:inventory][:title] }
+
+    if params[:inventory][:item_id].present?
+      selected_item = Item.find(params[:inventory][:item_id])
+      if selected_item.item_type == 'supply'
+        @inventory.unit = selected_item.unit
+      else
+        @inventory.unit = params[:inventory][:unit]
+      end
+    else
+      @inventory.unit = params[:inventory][:unit]
+    end
 
     if @inventory.save
       redirect_to stocks_path, notice: "Inventory item added successfully."
     else
       @items = Item.all
       @suppliers = Supplier.all
+      @brands = Brand.all
       flash.now[:alert] = "Failed to add inventory item. Please check the form."
       render :new
     end
   end
 
   def edit
-    @inventory = Inventory.find(params[:id])
     @items = Item.all
     @suppliers = Supplier.all
-    render :new 
+    @brands = Brand.all
+    render :new
   end
-  
-  def update
 
-    @inventory.data = { "title" => params[:inventory][:title] }
+  def update
+    @inventory.data = {
+      "title" => params[:inventory][:title],
+      "brand_id" => params[:inventory][:brand_id],
+      "model" => params[:inventory][:model],
+      "threshhold_alerts" => params[:inventory][:threshhold_alerts],
+      "reorder_points" => params[:inventory][:reorder_points]
+    }
+
+    if params[:inventory][:item_id].present?
+      selected_item = Item.find(params[:inventory][:item_id])
+      if selected_item.item_type == 'supply'
+        @inventory.unit = selected_item.unit
+      else
+        @inventory.unit = params[:inventory][:unit]
+      end
+    else
+      @inventory.unit = params[:inventory][:unit]
+    end
 
     if @inventory.update(inventory_params)
       redirect_to stocks_path, notice: "Inventory item updated successfully."
     else
       @items = Item.all
       @suppliers = Supplier.all
+      @brands = Brand.all
       flash.now[:alert] = "Failed to update inventory item. Please check the form."
       render :edit
     end
@@ -82,11 +119,11 @@ class StocksController < ApplicationController
       else
         redirect_to stocks_path, alert: "Failed to delete inventory item."
       end
-    rescue ActiveRecord::InvalidForeignKey => e
-      redirect_to stocks_path, alert: "Unable to delete. This inventory is already used in another module."
+    rescue ActiveRecord::InvalidForeignKey
+      redirect_to stocks_path, alert: "Unable to delete. This item is already used in another module."
     end
   end
-  
+
   private
 
   def set_inventory
@@ -102,7 +139,8 @@ class StocksController < ApplicationController
       :unit,
       :quantity,
       :purchase_date,
-      :status
+      :status,
+      :data
     )
   end
 end
